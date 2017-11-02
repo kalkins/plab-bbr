ccfc1d78b4dd5c7616bb8c1be51dbd24a2bab1b7
Sindre Stephansen <sindre@sindrestephansen.com>
2017-11-02 12:55:41 +0100

    Update Behaviour __init__ arguments

--- behaviour.py
+++ behaviour.py
@@ -1,13 +1,21 @@
 class Behaviour:
     """A behaviour analyzes some sensors and determines what the motors should do."""
 
-    def __init__(self):
-        self.bbcon = None
-        self.sensobs = []
+    def __init__(self, bbcon, sensobs, priority):
+        """
+        Initialize the Behaviour object.
+
+        Arguments:
+        bbcon    -- The Behaviour-Based controller object
+        sensobs  -- A dictionary of sensobs that the behaviour will use
+        priority -- The priority of the behaviour
+        """
+        self.bbcon = bbcon
+        self.sensobs = sensobs
         self.motor_recommendations = []
         self.active_flag = False
         self.halt_request = False
-        self.priority = 0
+        self.priority = priority
         self.match_degree = 0
         self.weight = 0
 
