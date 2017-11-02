9d37e93b93f0ae158a8fe1d32d647bc65e2f0809
Markus Rekdal <markus.rekdal@gmail.com>
2017-11-02 17:11:06 +0100

    Enable motob to work with multible operations

--- motob.py
+++ motob.py
@@ -22,22 +22,23 @@ class MainMotob(Motob):
         self.value = rec
 
     def operationalize(self):
-        direction = self.value[0].lower()
-        speed = self.value[1]
-        if speed == -1:
-            speed = 0.25
-        angle = self.value[2]
-        duration = self.value[3]
-        if duration == -1:
-            duration = 0.5  # To be replaced with potential timestep-time
-        turntime = angle/180 * 1/speed * 0.2  # 0.2 is a guess, and must be adjusted
-        if direction == "f":
-            self.motor.forward(speed, duration)
-        elif direction == "r":
-            self.motor.backward(speed, duration)
-        elif direction == "l":
-            self.motor.left(speed, turntime)
-        elif direction == "r":
-            self.motor.right(speed, turntime)
-        elif direction == "s":
-            self.motor.stop()
+        for operation in self.value:
+            direction = operation[0].lower()
+            speed = operation[1]
+            if speed == -1:
+                speed = 0.25
+            angle = operation[2]
+            duration = operation[3]
+            if duration == -1:
+                duration = 0.5  # To be replaced with potential timestep-time
+            turntime = angle/180 * 1/speed * 0.2  # 0.2 is a guess, and must be adjusted
+            if direction == "f":
+                self.motor.forward(speed, duration)
+            elif direction == "r":
+                self.motor.backward(speed, duration)
+            elif direction == "l":
+                self.motor.left(speed, turntime)
+            elif direction == "r":
+                self.motor.right(speed, turntime)
+            elif direction == "s":
+                self.motor.stop()
