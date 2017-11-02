052d886dbc1939cd50f189ccc650a3b1c46d2af7
Sindre Stephansen <sindre@sindrestephansen.com>
2017-11-02 18:59:27 +0100

    Modify ExploreBehaviour so that it's always active

--- explore_behaviour.py
+++ explore_behaviour.py
@@ -4,23 +4,21 @@ from behaviour import Behaviour
 
 
 class ExploreBehaviour(Behaviour):
+    active_flag = True
+
     def consider_deactivation(self):
-        if self.active_flag:
-            if self.senobs["ultrasonic"].get_is_detected():
-                self.active_flag = False
+        pass
 
     def consider_activiation(self):
-        if not self.active_flag:
-            if not self.senobs["ultrasonic"].get_is_detected():
-                self.active_flag = True
+        pass
 
     def sense_and_act(self):
-        if self.active_flag:
-            self.match_degree = 0.1
-            dirNumber = random.randint(0, 2)
-            if dirNumber == 0:
-                self.motor_recommendations = [(("f", 0.4, 0, -1))]
-            elif dirNumber == 1:
-                self.motor_recommendations = [("l", 0.4, 45, -1)]
-            else:
-                self.motor_recommendations = [("r", 0.4, 45, -1)]
+        self.match_degree = 0.1
+        dirNumber = random.randint(0, 2)
+
+        if dirNumber == 0:
+            self.motor_recommendations = [("f", 0.4, 0, -1)]
+        elif dirNumber == 1:
+            self.motor_recommendations = [("l", 0.4, 45, -1)]
+        else:
+            self.motor_recommendations = [("r", 0.4, 45, -1)]
