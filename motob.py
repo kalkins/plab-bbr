330a72db45c1fe79b98e22a29f94ce26039f0a04
Markus Rekdal <markus.rekdal@gmail.com>
2017-11-02 12:47:46 +0100

    Add motob for controlling the wheels

--- motob.py
+++ motob.py
@@ -1,8 +1,10 @@
+from motors import Motors
+
 class Motob:
     """An interface one or more motors."""
 
     def __init__(self):
-        self.motors = []
+        self.motor = Motors()
         self.value = None
 
     def update(self, rec):
@@ -12,3 +14,30 @@ class Motob:
     def operationalize(self, rec):
         """Convert a motor recommendation to one or more motor settings."""
         pass
+
+
+class MainMotob(Motob):
+
+    def update(self, rec):
+        self.value = rec
+
+    def operationalize(self, rec):
+        direction = rec[0].lower()
+        speed = rec[1]
+        if speed == -1:
+            speed = 0.25
+        angle = rec[2]
+        duration = rec[3]
+        if duration == -1:
+            duration = 0.5  # To be replaced with potential timestep-time
+        turntime = angle/180 * 1/speed * 0.2  # 0.2 is a guess, and must be adjusted
+        if direction == "f":
+            self.motor.forward(speed, duration)
+        elif direction == "r":
+            self.motor.backward(speed, duration)
+        elif direction == "l":
+            self.motor.left(speed, turntime)
+        elif direction == "r":
+            self.motor.right(speed, turntime)
+        elif direction == "s":
+            self.motor.stop()
