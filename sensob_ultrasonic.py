119c5856d39da2962f3ddbb3079eaf75e74d87bb
jvmanne <jenny.v.manne@gmail.com>
2017-11-02 12:29:48 +0100

    Add sensob Ultrasonic class

---
+++ sensob_ultrasonic.py
@@ -0,0 +1,23 @@
+from sensors.ultrasonic import Ultrasonic
+from sensob import Sensob
+
+
+class SensobUltrasonic(Sensob):
+
+    def __init__(self):
+        super().__init__()
+        self.sensor = Ultrasonic()
+        self.is_detected = False
+        self.distance = 0
+
+    def update(self):
+        self.sensor.update()
+        self.distance = self.sensor.get_value()
+        self.is_detected = self.distance < 50
+
+    def get_is_detected(self):
+        return self.is_detected
+
+    def get_distance(self):
+        return self.distance
+
