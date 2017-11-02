fa69080cef43571f268c0b482392ad40895aefe6
Sindre Stephansen <sindre@sindrestephansen.com>
2017-11-02 15:19:36 +0100

    Add an arbitrator that selects the behaviour with highest weight

--- arbitrator.py
+++ arbitrator.py
@@ -1,9 +1,26 @@
 class Arbitrator:
     """A class that decide which behaviour recommendation to follow."""
 
-    def __init__(self):
-        self.bbcon = None
+    def __init__(self, bbcon, behaviours):
+        self.bbcon = bbcon
+        self.behaviours = behaviours
 
     def choose_action(self):
         """Decide which behaviour to use."""
         pass
+
+
+class HighestArbitrator(Arbitrator):
+    def choose_action(self):
+        number = 0
+        halt = False
+
+        for behaviour in self.behaviours:
+            if behaviour.halt_request:
+                halt = True
+
+            if number > behaviour.weight:
+                number = behaviour.weight
+                correctBehaviour = behaviour
+
+        return (correctBehaviour.motor_recommendations[0], halt)
