a122d9f4df1776835c42be17ffa329084c3bdf0f
Sindre Stephansen <sindre@sindrestephansen.com>
2017-11-06 09:38:53 +0100

    Fix AttackBehaviour activation/deactivation requirements

--- attack_behaviour.py
+++ attack_behaviour.py
@@ -4,14 +4,13 @@ from behaviour import Behaviour
 class AttackBehaviour(Behaviour):
     def consider_deactivation(self):
         ultraSonic = self.sensobs["ultrasonic"]
-        greenCamera = self.sensobs["greencamera"]
-        if ultraSonic.get_is_detected() and greenCamera.value < 0.5:
+        if not ultraSonic.get_is_detected():
             self.active_flag = False
 
     def consider_activation(self):
         ultraSonic = self.sensobs["ultrasonic"]
         redCamera = self.sensobs["redcamera"]
-        if not ultraSonic.get_is_detected() and redCamera.value > 0.7:
+        if ultraSonic.get_is_detected() and redCamera.value > 0.7:
             self.active_flag = True
 
     def sense_and_act(self):
